787242fb723d82a9501a47281e65bb2aebfc1fbe
Christopher Artis <mrchrisomat@gmail.com>
2023-03-09 16:12:40 -0500

    working

--- site.js
+++ site.js
@@ -1,12 +1,35 @@
 const audioContext = new AudioContext()
-let playing = false
-let play_button = document.getElementById("play_button")
-let input = document.getElementById("in")
+var playing = false
+var play_button = document.getElementById("play_button")
+var input = document.getElementById("in")
+var audio 
+var track
+
+
+function checkInput(){ 
+    let uploadInput = input.files[0]
+    let link = URL.createObjectURL(uploadInput)
+
+    audio = new Audio(link)
+
+    track = audioContext.createMediaElementSource(audio)
+    track.connect(audioContext.destination)
+}
+
 function play() {
 
-    //let a = new Audio(input.attributes.)
-    console.log(input.getAttribute("value"))
-    audioContext.createMediaElementSource()
+    if (audioContext.state == "suspended") {
+        audioContext.resume()
+    }
+    
+    checkInput()
+    audio.play()
+
+}
+
+function stop(){ 
+    audio.pause()
+
 }
 function click1() {
     if (playing == false) {
@@ -16,5 +39,6 @@ function click1() {
     } else {
         playing = false
         play_button.innerText = "Play"
+        stop()
     }
 }
