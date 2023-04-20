683a2941142739c91e6faba87d6be2ecbcf5fa0c
Daniel Beristain <djberistain0@gmail.com>
2023-04-19 20:18:41 -0400

    created database-interacting functions

---
+++ scripts/api/createUser.js
@@ -0,0 +1,18 @@
+import { query } from '../../lib/db';
+
+export default async function handler(req, res) {
+  const { name, email, password } = req.body;
+
+  try {
+    const results = await query(
+      `
+      INSERT INTO sys.users (username, password, email)
+      VALUES (?, ?, ?)
+      `,
+      [username, email, password]
+    );
+    return res.status(200).json({ message: 'User created successfully' });
+  } catch (e) {
+    return res.status(500).json({ message: e.message });
+  }
+}
